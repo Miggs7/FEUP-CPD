924902f9ad7cee9519ca766d2f2f61fc011f3111
n00n <up201807530@fe.up.pt>
2023-05-08 20:36:41 +0100

    Complemented Player.java

--- assign2/src/model/Player.java
+++ assign2/src/model/Player.java
@@ -1,12 +1,72 @@
 package model;
 
 import java.net.*;
+import java.util.*;
+import java.util.LinkedList;
+
 
 public class Player{
     private String playerName = null;
     private boolean MyTurn = false;
     private boolean saidUNO = false;
-    private List
+    private LinkedList<String> myCards; //to Do String as a placeholder for UNOcard
+
+
+    public Player(String name){
+        setName(name);
+        myCards = new LinkedList<String>();
+    }
+
+    public void setName(String name){
+        playerName = name;
+    }
+
+    public String getName(){
+        return this.playerName;        
+    }
+
+    public int getNumCards(){
+        return myCards.size();
+    }
+
+    public boolean isMyturn(){
+        return MyTurn;
+    }
+
+    public void setMyTurn(){
+        if(!MyTurn){
+            MyTurn = true;
+        }
+        else{
+            MyTurn = false;
+        }
+    }
+
+    public boolean hasCards(){
+        if (myCards.isEmpty()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public boolean getSaidUNO(){
+        return saidUNO;
+    }
+
+    public void saidUNO(){
+        saidUNO = true;
+    }
+    
+    public void dontSaidUNO(){
+        saidUNO = false;
+    }
+
+
+
+
+
     
 
     
