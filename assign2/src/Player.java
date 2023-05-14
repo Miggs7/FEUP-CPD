b14d6bfb1673fefbfe56a51bc9d656624555ba8f
n00n <up201807530@fe.up.pt>
2023-05-14 15:41:02 +0100

    Some changes

---
+++ assign2/src/Player.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.util.*;
+
+
+public class Player{
+    private int playerID;
+    private String playerName = null;
+    private int playerScore = 0;
+
+
+    public Player(String name, int id){
+        setId(id);
+        setName(name);
+    }
+
+    public void setName(String name){
+        this.playerName = name;
+    }
+
+    public void setId(int id){
+        this.playerID = id;
+    }
+
+    public int getId(){
+        return this.playerID;
+    }
+
+    public String getName(){
+        return this.playerName;        
+    }
+
+    public void addScore(int num){
+        this.playerScore += num;
+    }
+
+    public int getScore(){
+        return this.playerScore;
+    }
+}
